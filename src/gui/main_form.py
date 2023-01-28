e2b418ac5250f2ebb6a6033a6beaec6052cae32a
edzvonik <ev.dzvonik@gmail.com>
2023-01-28 16:04:34 +0300

    Refactor main form

--- src/gui/main_form.py
+++ src/gui/main_form.py
@@ -4,64 +4,72 @@ from tkinterdnd2 import DND_FILES, TkinterDnD
 
 class Main:
     def __init__(self):
+        
+        # Root
         self.root = TkinterDnD.Tk()
-
-        #setting title
         self.root.title("Squeezer")
-        #setting window size
         width=460
-        height=350
+        height=400
         screenwidth = self.root.winfo_screenwidth()
         screenheight = self.root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.root.geometry(alignstr)
         self.root.resizable(width=False, height=False)
 
-        self.button=tk.Button(self.root)
-        self.button["bg"] = "#e9e9ed"
-        ft = tkFont.Font(family="Monospace",size=16)
-        self.button["font"] = ft
-        self.button["fg"] = "#000000"
-        self.button["justify"] = "center"
-        self.button["text"] = "Сжать"
-        self.button.place(x=10,y=270,width=440,height=65)
+        # Buttons 
+        button_font = tkFont.Font(family="Monospace",size=8)
 
-        self.listbox=tk.Listbox(self.root)
-        self.listbox["borderwidth"] = "1px"
-        ft = tkFont.Font(family="Monospace", size=5)
-        self.listbox["font"] = ft
-        self.listbox["fg"] = "#333333"
-        self.listbox["justify"] = "left"
+        self.delete_button = tk.Button(
+            self.root, text = "Удалить", 
+            command = self.delete_button_click, 
+            font = button_font
+            )
+        self.delete_button.place(x = 10, y = 270, width = 214, height = 41)
+
+        self.deleteAll_button = tk.Button(
+            self.root, text = "Удалить всё", 
+            command = self.deleteAll_button_click, 
+            font = button_font)
+        self.deleteAll_button.place(x = 230, y = 270, width = 222, height = 41)
+
+        self.button=tk.Button(
+            self.root, 
+            text = "Сжать!", 
+            font = button_font, 
+            justify = "center", 
+            command = self.button_click
+            )
+        self.button.place(x = 10, y = 320, width = 440, height = 65)
+
+        # ListBox
+        listbox_font = tkFont.Font(family="Monospace", size=5)
+
+        self.listbox=tk.Listbox(
+            self.root, 
+            selectmode = tk.MULTIPLE, 
+            font = listbox_font,
+            justify = "left",
+            borderwidth = "1px"
+            )
         self.listbox.place(x=10,y=10,width=440,height=249)
-        self.listbox.insert(1, "Перенесите сюда файлы для сжатия")
+        self.listbox.insert("end", "Перенесите сюда файлы для сжатия")
         self.listbox.drop_target_register(DND_FILES)
         self.listbox.dnd_bind("<<Drop>>", self.add_paths)
-        # self.listbox.pack(pady = 5)
 
         self.root.mainloop()
 
     def button_click(self):
-        pass
+        print("Click!")
+
+    def delete_button_click(self):
+        selected_checkboxs = self.listbox.curselection()
+        for selected_checkbox in selected_checkboxs[::-1]:
+            self.listbox.delete(selected_checkbox)
+
+    def deleteAll_button_click(self):
+        self.listbox.delete(0, "end")
 
     def add_paths(self, event):
-        paths = event.data.split(" ")
-        for path in paths:
-            self.listbox.insert("end", path)
-
-    # if __name__ == "__main__":
-    #     root = tk.Tk()
-    #     app = App(root)
-    #     root.mainloop()
-
-    # def __init__(self):
-    #     self.__main = TkinterDnD.Tk()
-    #     self.__main.minsize(450, 300)
-
-    #     self.__listbox = tk.Listbox(__main)
-    #     self.__listbox.insert(1, "Перенесите файлы для сжатия")
-
-    #     self.__listbox.drop_target_register(DND_FILES)
-    #     self.__listbox.dnd_bind("<<Drop>>", lambda e : __listbox.insert(tk.END, e.data))
-    #     self.__listbox.pack(pady = 5)
-
-    #     self.__main.mainloop()
+        if (self.listbox.get(0) == "Перенесите сюда файлы для сжатия"):
+            self.listbox.delete(0, "end")
+        self.listbox.insert("end", event.data)
