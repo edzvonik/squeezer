a2ed4717ee7fd917033b3f64552b31eaa46b7dcb
edzvonik <ev.dzvonik@gmail.com>
2023-01-30 22:08:09 +0300

    Refactor main_form.py

--- src/gui/main_form.py
+++ src/gui/main_form.py
@@ -14,16 +14,16 @@ class Main:
         rootPath = Path(__file__).parent.parent.parent
         iconPath = Path.joinpath(rootPath, "squeezer.png")
         self.root.iconphoto(True, tk.PhotoImage(file=iconPath))
-        width=460
-        height=400
+        width = 460
+        height = 400
         screenwidth = self.root.winfo_screenwidth()
         screenheight = self.root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.root.geometry(alignstr)
-        self.root.resizable(width=False, height=False)
+        self.root.resizable(width = False, height = False)
 
         # Buttons 
-        button_font = tkFont.Font(family="Monospace",size=8)
+        button_font = tkFont.Font(family = "Monospace", size = 8)
 
         self.delete_button = tk.Button(
             self.root, text = "Удалить", 
@@ -48,16 +48,16 @@ class Main:
         self.button.place(x = 10, y = 320, width = 440, height = 65)
 
         # ListBox
-        listbox_font = tkFont.Font(family="Monospace", size=5)
+        listbox_font = tkFont.Font(family = "Monospace", size = 5)
 
-        self.listbox=tk.Listbox(
+        self.listbox = tk.Listbox(
             self.root, 
             selectmode = tk.MULTIPLE, 
             font = listbox_font,
             justify = "left",
             borderwidth = "1px"
             )
-        self.listbox.place(x=10,y=10,width=440,height=249)
+        self.listbox.place(x = 10, y = 10, width = 440, height = 249)
         self.listbox.insert("end", "Перенесите сюда файлы для сжатия")
         self.listbox.drop_target_register(DND_FILES)
         self.listbox.dnd_bind("<<Drop>>", self.add_paths)
@@ -72,8 +72,7 @@ class Main:
             clearPath = path.replace("{", "").replace("}", "")
             im = Image.open(clearPath)
             sq.Squeezer.squeeze(im, clearPath, sq.Squeezer.FILE_SIZE)
-
-
+            
     def delete_button_click(self):
         selected_checkboxs = self.listbox.curselection()
         for selected_checkbox in selected_checkboxs[::-1]:
